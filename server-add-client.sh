60160e4ed3f40740e2e2008cba649da792deeb0f
Keneth Lallemand <kenallemand@hotmail.com>
2018-10-28 19:11:29 -0500

    Modificacion add client

--- server-add-client.sh
+++ server-add-client.sh
@@ -11,13 +11,12 @@ read -p "nombre pc cliente en el cluster (coloca un nombre unico para evitar col
 echo "$ip_local	$nombre_pc" >> /etc/hosts
 echo "Agregado el cliente al archivo hosts para usar el nombre en lugar de su ip local :)"
 #se agrega la ip del cliente en los permitidos del nfs para compartir la carpeta
-echo "/nfs ${ip_local}(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+echo "/nfs $ip_local(rw,sync,no_root_squash,no_subtree_check)" >> /etc/exports
+exportfs -a
+systemctl restart nfs
 read -p "Introduzca la contraseña de root del cliente: " pass_ssh
-echo $pass_ssh
 #pd: se ejecutan los comandos por medio de ssh al cliente
-#Agregacion de 
 echo root@$ip_local
-ssh root@$ip_local 'whoami'
 sshpass -p $pass_ssh ssh -o StrictHostKeyChecking=no root@$ip_local 'adduser mpiuser'
 #se agrega el usuario para trabajar en el cluster
 echo "Agrega la contraseña para mpiuser(se recomienda usar mpiuser si es solo un cluster de demostracion): "
@@ -26,18 +25,19 @@ sshpass -p $pass_ssh ssh -o StrictHostKeyChecking=no root@$ip_local 'passwd mpiu
 sshpass -p $pass_ssh ssh -o StrictHostKeyChecking=no root@$ip_local 'echo "mpiuser   ALL=(ALL)   ALL" >> /etc/sudoers'
 #instalacion dependencias nfs
 sshpass -p $pass_ssh ssh -o StrictHostKeyChecking=no root@$ip_local 'yum -y install nfs-utils wget'
-sshpass -p $pass_ssh ssh -o StrictHostKeyChecking=no root@$ip_local 'mkdir -p /nfs; chmod -R 777 /nfs'
+sshpass -p $pass_ssh ssh -o StrictHostKeyChecking=no root@$ip_local 'mkdir -p /nfs'
 #se toma como predeterminada del servidor la direccion 10.0.1.2
 read -p "IP local del servidor (normalmente 10.0.1.2): " ip_pc
-sshpass -p $pass_ssh ssh -o StrictHostKeyChecking=no root@$ip_local 'showmount -e $ip_pc; rpcinfo -p $ip_pc'
+sshpass -p $pass_ssh ssh -o StrictHostKeyChecking=no root@$ip_local 'showmount -e $ip_pc ; rpcinfo -p $ip_pc'
 #montaje disco de red en carpeta local
-sshpass -p $pass_ssh ssh -o StrictHostKeyChecking=no root@$ip_local 'mount ${ip_pc}:/nfs /nfs'
+sshpass -p $pass_ssh ssh -o StrictHostKeyChecking=no root@$ip_local 'mount $ip_pc:/nfs /nfs'
 sshpass -p $pass_ssh ssh -o StrictHostKeyChecking=no root@$ip_local 'df -h'
-cd /nfs
-sshpass -p $pass_ssh ssh -o StrictHostKeyChecking=no root@$ip_local 'touch /nfs/sucess-${ip_local}'
-echo "$nombre_pc	${ip_local}" >> /nfs/hosts
+sshpass -p $pass_ssh ssh -o StrictHostKeyChecking=no root@$ip_local 'touch /nfs/sucess-$ip_local'
+echo "$nombre_pc	$ip_local" >> /nfs/hosts
 #se guarda para proximo montaje
-sshpass -p $pass_ssh ssh -o StrictHostKeyChecking=no root@$ip_local 'echo "${ip_pc}:/nfs /nfs nfs auto,noatime,nolock,bg,nfsvers=3,intr,tcp,actimeo=1800 0 0" >> /etc/fstab'
+sshpass -p $pass_ssh ssh -o StrictHostKeyChecking=no root@$ip_local 'echo "$ip_pc:/nfs /nfs nfs auto,noatime,nolock,bg,nfsvers=3,intr,tcp,actimeo=1800 0 0" >> /etc/fstab'
+sshpass -p $pass_ssh ssh -o StrictHostKeyChecking=no root@$ip_local 'mount -a'
+
 #configuracion de ssh
 #se copia desde el nfs la clave del servidor
 sshpass -p $pass_ssh ssh -o StrictHostKeyChecking=no root@$ip_local 'sudo -u mpiuser -H sh -c "mkdir /home/mpiuser/.ssh"'
